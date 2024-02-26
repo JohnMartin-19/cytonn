f2d67078eefc0299c4fa08dcbffdc00a07cf78d4
John <johnnkonge2020@gmail.com>
2024-02-26 22:07:43 +0300

    made migrations

--- Internship/backend/events/models.py
+++ Internship/backend/events/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=100)
+    location = models.CharField(max_length = 100)
+    image = models.ImageField(upload_to='event_images/')
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+    attendees = models.PositiveIntegerField(null = False)
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title + " | " + str(self.location)
